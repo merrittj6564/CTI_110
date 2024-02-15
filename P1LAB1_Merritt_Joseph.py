7aad8dfb0f82e96a9f9895304479cf7a6d159e65
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-02-15 10:35:38 -0500

    My first Python submission

---
+++ P1LAB1_Merritt_Joseph.py
@@ -0,0 +1,12 @@
+#Joseph Merritt
+# 2/13/2024
+# P1LAB1
+# Using input and output
+
+#Create a variable and get input from the user
+name = input("Enter your name: ")
+
+#Output a message using the variable
+print("Hey", name, "\nWelcome to Zybooks!")
+#print("Welcome to Zybooks!)
+      
