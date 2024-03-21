90b43caaac1ac297d3501fed4d81e218f98aa50d
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-03-21 14:32:29 +0000

    updated

--- P3HW2_Merritt_Joseph.py
+++ P3HW2_Merritt_Joseph.py
@@ -3,10 +3,13 @@
 #P3HW2
 #Salary
 
+var = input("Enter employee's name: ")
 var1 = float(input("Enter a value with a decimal: "))
 var2 = float(input("Enter a value with a decimal: "))
 
 print("Variable 1     Variable 2")
 print("--------------------------")
+print("Employee name: "(var))
 print(f'{var1} {var2}')
 print(f'{var1:<15.2f} {var2:<10.2f}')
+
