d4013c7951efcee46d219c358540dfe6d5e0cd4d
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-03-21 15:56:54 +0000

    updated

--- P3HW2_Merritt_Joseph.py
+++ P3HW2_Merritt_Joseph.py
@@ -4,12 +4,29 @@
 #Salary
 
 var = input("Enter employee's name: ")
-var1 = float(input("Enter a value with a decimal: "))
-var2 = float(input("Enter a value with a decimal: "))
+hrs_worked = float(input("Enter number of hours worked: "))
+pay_rate = float(input("Enter employee's pay rate: "))
 
-print("Variable 1     Variable 2")
+    
+
 print("--------------------------")
-print("Employee name: "(var))
-print(f'{var1} {var2}')
-print(f'{var1:<15.2f} {var2:<10.2f}')
+print("Employee name: ", var)
 
+print(f"\n{'Hours Worked':<15}{'Pay Rate':<10}{'OverTime':<10}{'OverTime Pay':<15}{'RegHour Pay':<15} {'Gross Pay':<10}")
+print("---------------------------------------------------------------------------")
+
+if hrs_worked >= 41:
+    reg_hrs = 40
+    OT_hrs = hrs_worked - 40
+else: #no overtime exists
+    reg_hrs = hrs_worked
+    OT_hrs = 0
+
+OTpay_rate = pay_rate * 1.5
+reg_pay = reg_hrs * pay_rate
+OT_pay = OT_hrs * OTpay_rate
+gross_pay = OT_pay + reg_pay
+
+    
+
+print(f'{hrs_worked:<15.2f} {pay_rate:<10.2f} {OT_hrs:<10.2f} {OT_pay:<15.2f}{reg_pay:<15.2f}{gross_pay:<15.2f}')
