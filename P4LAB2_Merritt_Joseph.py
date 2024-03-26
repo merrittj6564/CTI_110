a24e92627370704980ef66ad151e1f5488f69e5f
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-03-26 15:46:49 +0000

    updated

--- P4LAB2_Merritt_Joseph.py
+++ P4LAB2_Merritt_Joseph.py
@@ -2,15 +2,15 @@
 #26_Mar_24
 #P4LAB2 - Using the range function
 
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
+num1 = int(input("Enter a num1: "))
+num2 = int(input("Enter a num2: "))
 
 #While loop - as long as num1 is smaller than num2, increase by 5 each
 
 while num1 > num2: #Bad
     print("First number must be smaller")
-    num1 = int(input("Enter a number: "))
-    num2 = int(input("Enter a number: "))
+    num1 = int(input("Enter a num1: "))
+    num2 = int(input("Enter a num2: "))
 #Code will happen ONLY when the loop breaks
 print("You did it right!")    
         
