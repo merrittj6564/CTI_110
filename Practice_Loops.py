1607865cca2c6a9b90e13ccf1ed1808f0bcf2808
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-03-26 14:48:20 +0000

    updated

---
+++ Practice_Loops.py
@@ -0,0 +1,44 @@
+#loops
+
+#For Loop using range function
+...
+counter = 0
+for item in range(4):
+    print(counter)
+    counter += 2
+#Outside the loop
+print("The final value is", counter)    
+
+for item in range(4):
+    print (item+1)
+
+#Here, range is 0-3 we only have a stop value
+    for item in range(4):
+        counter += 2
+        print(counter)
+
+#Here range is 2-9,we give a start and stop value
+for num in range(2,10):
+    print(num)      
+
+
+stop_val = int( input("How many times do you want to run?"))
+for num in range(stop_val):
+    print("Program runs")
+...
+
+my_list = [2.2,6.5,8.34, 65.0]
+
+for num in my_list:
+    print(num)
+...
+#While loop runs until a specific condtion is met
+#Create variable to control loop
+
+run_again = "y"
+while run_again == "y":
+    print("\nProgram is running\n")
+    run_again = input("Would you like to run program again? y or n ")
+#At this point, loop has been broken
+print("Program has ended, goodbye!")    
+        
