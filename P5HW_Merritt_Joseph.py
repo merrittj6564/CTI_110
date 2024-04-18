6d6b1174fbe65b1a10f533a51b6ee018e83ffa23
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-04-18 15:49:15 +0000

    updated

--- P5HW_Merritt_Joseph.py
+++ P5HW_Merritt_Joseph.py
@@ -2,6 +2,14 @@
 #18_Apr_24
 #P5HW - Using Loops, Functions and Module Import
 
+import random
+
+def sum(num1, num2):
+    print(f"add {num1} ande {num2}")
+    result = num1 + num2
+
+
+
 print("Welcome to Math Quiz")
 print()
 print("MAIN MENU")
@@ -10,3 +18,14 @@ print("1. Adding Random Numbers")
 print("2. Subracting Random Numbers")
 print("3. Exit")
 print()
+
+menu_opt = int(input("Please choose one of the menu options: "))
+
+while menu_opt != 3:
+    if menu_opt == 1:
+        sum() 
+    if menu_opt == 2:
+        minus()
+    menu_opt = int(input("Please choose one of the menu options: "))
+#At this point, loop has been broken
+print("Program has ended, goodbye!") 
