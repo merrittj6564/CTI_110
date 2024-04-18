0c013b471bdd651739dc01d4e4dcb26de87ce7eb
merrittj6564 <160150964+merrittj6564@users.noreply.github.com>
2024-04-18 15:26:53 +0000

    updated

---
+++ P5HW_Merritt_Joseph.py
@@ -0,0 +1,12 @@
+#Joseph_Merritt
+#18_Apr_24
+#P5HW - Using Loops, Functions and Module Import
+
+print("Welcome to Math Quiz")
+print()
+print("MAIN MENU")
+print("----------------------")
+print("1. Adding Random Numbers")
+print("2. Subracting Random Numbers")
+print("3. Exit")
+print()
